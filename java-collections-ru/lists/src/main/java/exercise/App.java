40b6457766929a15ee8e276583eaf1e812a413c7
@hexlet/cli <support@hexlet.io>
2023-12-15 15:41:20 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -1,37 +1,31 @@
 package exercise;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 // BEGIN
 class App {
 
     public static boolean scrabble(String symbols, String word) {
-        if(word.length() > symbols.length()) {
-            return false;
-        }
-        List<Character> sList = new ArrayList<>();
-        for (char c : symbols.toCharArray()) {
-            sList.add(c);
-        }
-        List<Character> wList = new ArrayList<>();
-        for (char c : word.toCharArray()) {
-            wList.add(c);
-        }
-        int count = 0;
-        for (Character character : wList) {
-            String str1 = String.valueOf(character);
-            for (int j = 0; j < sList.size(); j++) {
-                String str2 = String.valueOf(sList.get(j));
-                if (str1.equalsIgnoreCase(str2)) {
-                    count++;
-                    sList.remove(sList.get(j));
-                    j = sList.size();
-                }
+        int length = word.length();
+        String[] letters = symbols.split("");
+        List list = new ArrayList(Arrays.asList(letters));
+
+        for (int i = 0; i < length; i++) {
+            String currentSymbol = word.substring(i, i + 1);
+
+            if(!list.contains(currentSymbol)) {
+                return false;
             }
+
+            list.remove(currentSymbol);
         }
-        return count == word.length();
+        return true;
     }
 
+    public static void main(String[] args) {
+        System.out.println(scrabble("rkqodlw", "woRld"));
+    }
 }
 //END
