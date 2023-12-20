47e9a2804bee97784763bf91559647b3b290a316
@hexlet/cli <support@hexlet.io>
2023-12-20 16:53:36 +0300

    submit java-collections-ru/generics

--- java-collections-ru/generics/src/main/java/exercise/App.java
+++ java-collections-ru/generics/src/main/java/exercise/App.java
@@ -7,7 +7,7 @@ import java.util.Map.Entry;
 class App {
     public static List<Map<String, String>> findWhere(List<Map<String, String>> booksList, Map<String, String> words) {
         var listBooks = new ArrayList<Map<String, String>>();
-        for(Map<String, String> map : booksList) {
+        for (Map<String, String> map : booksList) {
             var iteratorBooks = map.entrySet().iterator();
             var iteratorWord = words.entrySet().iterator();
             var entryWord = iteratorWord.next();
@@ -18,7 +18,7 @@ class App {
                     entryWord = iteratorWord.next();
                     continue;
                 }
-                if(entryBooks.getKey().equalsIgnoreCase(entryWord.getKey())
+                if (entryBooks.getKey().equalsIgnoreCase(entryWord.getKey())
                    && entryBooks.getValue().equalsIgnoreCase(entryWord.getValue()) && !iteratorWord.hasNext()) {
                     listBooks.add(map);
                 }
@@ -26,38 +26,6 @@ class App {
 
         }
         return listBooks;
-    }
-
-    public static void main(String[] args) {
-        List<Map<String, String>> books = new ArrayList<>();
-
-        Map<String, String> book1 = new HashMap<>(
-                Map.of("title", "Cymbeline", "author", "Shakespeare", "year", "1611")
-        );
-        Map<String, String> book2 = new HashMap<>(
-                Map.of("title", "Book of Fooos", "author", "FooBar", "year", "1111")
-        );
-        Map<String, String> book3 = new HashMap<>(
-                Map.of("title", "The Tempest", "author", "Shakespeare", "year", "1611")
-        );
-        Map<String, String> book4 = new HashMap<>(
-                Map.of("title", "Book of Foos Barrrs", "author", "FooBar", "year", "2222")
-        );
-        Map<String, String> book5 = new HashMap<>(
-                Map.of("title", "Still foooing", "author", "FooBar", "year", "3333")
-        );
-
-        books.add(book1);
-        books.add(book2);
-        books.add(book3);
-        books.add(book4);
-        books.add(book5);
-
-        Map<String, String> where = new HashMap<>(Map.of("author", "Shakespeare", "year", "1611"));
-
-        List<Map<String, String>> result = App.findWhere(books, where);
-
-        System.out.println(result); // =>
     }
 }
 //END
