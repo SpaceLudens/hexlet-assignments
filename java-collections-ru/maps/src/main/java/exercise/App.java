93f47492a993ad0133bb0ff4df6172799a84a30e
Rustam <SpaceLudens1@gmail.com>
2023-12-20 11:42:08 +0300

    add getWordCount and toString methods

---
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -0,0 +1,31 @@
+package exercise;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// BEGIN
+class App {
+    
+    public static Map<String, Integer> getWordCount(String str) {
+        String[] words = str.split(" ");
+        Map<String, Integer> map = new HashMap<>();
+        for(String st1 : words) {
+            int value = 1;
+            for(String st2 : words) {
+                if(st1.equalsIgnoreCase(st2)) {
+                    map.put(st1, value++);
+                }
+            }
+        }
+return map;
+    }
+
+    public static String toString(Map<String, Integer> map) {
+        StringBuilder result = new StringBuilder();
+        for(Map.Entry<String, Integer> item : map.entrySet()) {
+            result.append("  ").append(item.getKey()).append(": ").append(item.getValue()).append("\n");
+        }
+        return "{\n"+result+ "\n}";
+    }
+}
+//END
