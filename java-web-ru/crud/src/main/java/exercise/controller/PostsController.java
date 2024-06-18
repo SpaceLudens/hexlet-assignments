4933df41db67ca9db68cbaac505fa9259ccc9c0c
@hexlet/cli <support@hexlet.io>
2024-06-18 18:37:32 +0300

    submit java-web-ru/crud

--- java-web-ru/crud/src/main/java/exercise/controller/PostsController.java
+++ java-web-ru/crud/src/main/java/exercise/controller/PostsController.java
@@ -1,7 +1,6 @@
 package exercise.controller;
 
 import static io.javalin.rendering.template.TemplateUtil.model;
-import static org.eclipse.jetty.util.StringUtil.startsWithIgnoreCase;
 
 import exercise.dto.posts.PostsPage;
 import exercise.dto.posts.PostPage;
@@ -17,21 +16,21 @@ public class PostsController {
 
     // BEGIN
     public static void show(Context context) {
-       var id = context.pathParam("id");
-       var post = PostRepository
+        var id = context.pathParam("id");
+        var post = PostRepository
                .find(Long.valueOf(id))
                .orElseThrow(() -> new NotFoundResponse("Post with id = " + id + " not found"));
-       context.status(404);
-       var page = new PostPage((post));
-       context.render("posts/show.jte", model("page", page));
+        context.status(404);
+        var page = new PostPage((post));
+        context.render("posts/show.jte", model("page", page));
     }
 
     public static void index(Context context) {
-        var pageNumber = context.queryParamAsClass("pageNumber", Integer.class).getOrDefault(1);
+        var page = context.queryParamAsClass("page", Integer.class).getOrDefault(1);
         var pageSize = 5;
-        List<Post> posts =  PostRepository.findAll(pageNumber,pageSize);
-        var page = new PostsPage(posts, pageNumber);
-        context.render("posts/index.jte", model("page",page));
+        List<Post> posts =  PostRepository.findAll(page, pageSize);
+        var currentPage = new PostsPage(posts, page);
+        context.render("posts/index.jte", model("page", currentPage));
     }
     // END
 }
