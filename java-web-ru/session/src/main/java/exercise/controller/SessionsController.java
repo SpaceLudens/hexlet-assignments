a7194794cf6ce2aa0566b5e2a3b931cd52e78f01
@hexlet/cli <support@hexlet.io>
2024-07-18 21:17:17 +0300

    submit java-web-ru/session

--- java-web-ru/session/src/main/java/exercise/controller/SessionsController.java
+++ java-web-ru/session/src/main/java/exercise/controller/SessionsController.java
@@ -3,11 +3,8 @@ package exercise.controller;
 import static io.javalin.rendering.template.TemplateUtil.model;
 import exercise.dto.MainPage;
 import exercise.dto.LoginPage;
-import exercise.model.User;
 import exercise.repository.UsersRepository;
-import static exercise.util.Security.encrypt;
 
-import exercise.util.Generator;
 import exercise.util.NamedRoutes;
 import exercise.util.Security;
 import io.javalin.http.Context;
