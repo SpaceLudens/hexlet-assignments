b35f61fdd00d5669ee6eecd8a3a7f9e8da65e836
@hexlet/cli <support@hexlet.io>
2024-11-11 16:23:52 +0500

    submit java-spring-ru/beans

---
+++ java-spring-ru/beans/src/main/java/exercise/controller/WelcomeController.java
@@ -0,0 +1,27 @@
+package exercise.controller;
+
+import exercise.daytime.Daytime;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+// BEGIN
+@RestController
+public class WelcomeController {
+
+    @Autowired
+    private Daytime daytime;
+
+    @GetMapping("/welcome")
+    public String welcome() {
+
+        var currentDayRime = daytime.getName();
+
+        if (currentDayRime.equals("day")) {
+            return "It is " + currentDayRime  + "now! Welcome to Spring!";
+        } else {
+            return "It is " + currentDayRime  + "now! Welcome to Spring!";
+        }
+    }
+}
+// END
